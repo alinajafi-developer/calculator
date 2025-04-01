8382dc46dc5bb4184cdabe51e981cd4991023171
Ali Najafi <147813870+alinajafi-developer@users.noreply.github.com>
2025-04-02 01:36:40 +0330

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,154 +1,96 @@
-let num1 = ''
-let num2 = ''
-let sum = document.querySelector('#sum')
-let sub = document.querySelector('#sub')
-let mul = document.querySelector('#mul')
-let div = document.querySelector('#div')
-let _show = document.querySelector('#show')
-let _span = _show.children
 let sumFlag = false
 let subFlag = false
 let mulFlag = false
 let divFlag = false
-// let flag = true
+let num1 = ''
+let num2 = ''
+let keyVal = ''
+let _show = document.querySelector('#show')
 
 function nums(self) {
-
-
-
-
-    if (sumFlag == false && subFlag == false && mulFlag == false && divFlag == false) {
-        num1 += self.innerText
-        num1 = parseFloat(num1)
-        _span[0].innerHTML = num1
-
-    } else if (sumFlag || subFlag || mulFlag || divFlag) {
-        num2 += self.innerText
-        num2 = parseFloat(num2)
-        _span[2].innerHTML = num2
-    }
+    keyVal = self.innerText
+    _show.innerHTML += keyVal
 
 }
+
+
 
 
 function op(self) {
-    self = self.innerText
+    let _op = self.innerText
+    if (_show.innerText == '' || _show.innerText == null || _show.innerText == _op) {
+        alert('ابتدا یک عدد وارد کنید!')
+
+    } else {
 
 
-
-    if (self == '+') {
-        if (_span[0].innerText == '' || _span[0].innerText == null || subFlag == true || mulFlag == true || divFlag == true) {
-            alert('مشکلی وجود دارد یا اعداد خالی اند یا همزمان دو اپراتور کلیک شده است!')
+        if (_show.innerText.substr(-1) == '+' || _show.innerText.substr(-1) == '-' || _show.innerText.substr(-1) == '*' || _show.innerText.substr(-1) == '/') {
+            alert('نمی توان همزمان دو عملگر وارد نمود!')
         } else {
-            if (_span[1].innerText.indexOf('+') > -1) {
+            if (_op == '+') {
+                sumFlag = true
+                _show.innerHTML += '+'
+                subFlag = false
+                mulFlag = false
+                divFlag = false
 
-                alert('دو تا + زدی')
+            } else if (_op == '-') {
 
-            } else {
-                _span[1].innerHTML = '+'
-                sumFlag = true;
+                subFlag = true
+                _show.innerHTML += '-'
+                sumFlag = false
+                mulFlag = false
+                divFlag = false
+
+            } else if (_op == '*') {
+
+                mulFlag = true
+                _show.innerHTML += '*'
+                sumFlag = false
+                subFlag = false
+                divFlag = false
+            } else if (_op == '/') {
+
+                divFlag = true
+                _show.innerHTML += '/'
+                sumFlag = false
+                subFlag = false
+                mulFlag = false
+            }
+            else {
+                sumFlag = false
+                subFlag = false
+                mulFlag = false
+                divFlag = false
             }
         }
-    }
 
-    if (self == '-') {
-        if (_span[0].innerText == '' || _span[0].innerText == null || sumFlag == true || mulFlag == true || divFlag == true) {
-            alert('مشکلی وجود دارد یا اعداد خالی اند یا همزمان دو اپراتور کلکیک شده است!')
-        } else {
-            if (_span[1].innerText.indexOf('-') > -1) {
-
-                alert('دو تا زدی')
-
-
-            } else {
-                _span[1].innerHTML = '-'
-                subFlag = true;
-            }
-        }
-    }
-    if (self == '*') {
-        if (_span[0].innerText == '' || _span[0].innerText == null || sumFlag == true || subFlag == true || divFlag == true) {
-            alert('مشکلی وجود دارد یا اعداد خالی اند یا همزمان دو اپراتور کلکیک شده است!')
-        } else {
-            if (_span[1].innerText.indexOf('*') > -1) {
-
-                alert('دو تا زدی')
-
-
-            } else {
-                _span[1].innerHTML = '*'
-                mulFlag = true;
-            }
-        }
-    }
-    if (self == '/') {
-        if (_span[0].innerText == '' || _span[0].innerText == null || sumFlag == true || subFlag == true || mulFlag == true) {
-            alert('مشکلی وجود دارد یا اعداد خالی اند یا همزمان دو اپراتور کلکیک شده است!')
-        } else {
-            if (_span[1].innerText.indexOf('/') > -1) {
-                console.log(_span[1].innerText);
-
-                alert('دو تا زدی')
-
-
-            } else {
-                _span[1].innerHTML = '/'
-                divFlag = true;
-            }
-        }
     }
 
 }
+
 
 
 function eq() {
-
-    if (sumFlag) {
-        temp = parseFloat(num1) + parseFloat(num2)
-
-
-    }
-    if (subFlag) {
-        temp = parseFloat(num1) - parseFloat(num2)
-
-
-    }
-    if (mulFlag) {
-        temp = parseFloat(num1) * parseFloat(num2)
-
-
-    }
-    if (divFlag) {
-        temp = parseFloat(num1) / parseFloat(num2)
-
-
+    let result = _show.innerHTML
+    if (result.substr(-1) == '-' || result.substr(-1) == '+' || result.substr(-1) == '*' || result.substr(-1) == '/') {
+        alert('آخرین ورودی شما یک عملگر است!')
+    } else {
+        result = eval(result)
+        alert('جواب شما:  ' + result)   
+        _show.innerHTML = ''
     }
 
 
-    alert(temp)
-
-    num1 = ''
-    num2 = ''
-    _span[0].innerText = ''
-    _span[1].innerText = ''
-    _span[2].innerText = ''
-    sumFlag = false
-    subFlag = false
-    mulFlag = false
-    divFlag = false
-
-
-
+    
 }
 
+
+
 document.querySelector('#clear').addEventListener('click', function () {
-    num1 = ''
-    num2 = ''
-    _span[0].innerText = ''
-    _span[1].innerText = ''
-    _span[2].innerText = ''
-    sumFlag = false
-    subFlag = false
-    mulFlag = false
-    divFlag = false
+    _show.innerHTML = ''
 })
+
+
+
+
